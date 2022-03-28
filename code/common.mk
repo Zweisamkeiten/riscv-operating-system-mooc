9069fb9f88517e30835c3778d97809010d486dbc
Einsam <snitxmhm@gmail.com>
2022-03-28 21:38:52 +0800

    first commit to fit my computer

--- code/common.mk
+++ code/common.mk
@@ -1,10 +1,11 @@
-CROSS_COMPILE = riscv64-unknown-elf-
+CROSS_COMPILE = /home/einsam/p/riscv-operating-system-mooc/tools/gcc/bin/riscv64-unknown-elf-
 CFLAGS = -nostdlib -fno-builtin -march=rv32ima -mabi=ilp32 -g -Wall
 
 QEMU = qemu-system-riscv32
 QFLAGS = -nographic -smp 1 -machine virt -bios none
 
-GDB = gdb-multiarch
+#GDB = gdb-multiarch
+GDB = ${CROSS_COMPILE}gdb
 CC = ${CROSS_COMPILE}gcc
 OBJCOPY = ${CROSS_COMPILE}objcopy
 OBJDUMP = ${CROSS_COMPILE}objdump
