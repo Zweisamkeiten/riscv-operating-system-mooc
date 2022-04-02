c4ac5280c2f5d293cfe9f2324520c48de1c340a9
Einsam <snitxmhm@gmail.com>
2022-04-02 19:01:38 +0800

    exercise 7-2: 轮询方式读取控制台上输入的字符并回显

--- code/os/01-helloRVOS/kernel.c
+++ code/os/01-helloRVOS/kernel.c
@@ -1,11 +1,14 @@
 extern void uart_init(void);
+extern void uart_getc(void);
 extern void uart_puts(char *s);
 
 void start_kernel(void)
 {
-	uart_init();
-	uart_puts("Hello, RVOS!\n");
+  uart_init();
+  uart_puts("Hello, RVOS!\n");
 
-	while (1) {}; // stop here!
+  while (1) {
+    uart_getc();
+  }; // stop here!
 }
 
