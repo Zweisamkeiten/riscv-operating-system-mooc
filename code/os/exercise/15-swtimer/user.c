79bd20b64d5f227488191faec7653bee7c94f696
Einsam <snitxmhm@gmail.com>
2022-04-17 12:01:25 +0800

    练习15-1.1: 软件定时器数组管理

--- code/os/exercise/15-swtimer/user.c
+++ code/os/exercise/15-swtimer/user.c
@@ -2,21 +2,52 @@
 
 #define DELAY 4000
 
-#define USE_LOCK
+//#define USE_LOCK
+struct userdata {
+	int counter;
+	char *str;
+};
+
+struct userdata person = {0, "Jack"};
+
+void timer_func(void *arg)
+{
+	if (NULL == arg)
+		return;
+
+	struct userdata *param = (struct userdata *)arg;
+
+	param->counter++;
+	printf("=====> TIMEOUT: %s: %d\n", param->str, param->counter);
+}
 
 void user_task0(void)
 {
 	uart_puts("Task 0: Created!\n");
+
+	struct timer *t1 = timer_create(timer_func, &person, 3);
+	if (NULL == t1) {
+		printf("timer_create() failed!\n");
+	}
+	struct timer *t2 = timer_create(timer_func, &person, 5);
+	if (NULL == t2) {
+		printf("timer_create() failed!\n");
+	}
+	struct timer *t3 = timer_create(timer_func, &person, 7);
+	if (NULL == t3) {
+		printf("timer_create() failed!\n");
+	}
 	while (1) {
 #ifdef USE_LOCK
 		spin_lock();
 #endif
-		uart_puts("Task 0: Begin ... \n");
-		for (int i = 0; i < 5; i++) {
-			uart_puts("Task 0: Running... \n");
-			task_delay(DELAY);
-		}
-		uart_puts("Task 0: End ... \n");
+		uart_puts("Task 0: Running ... \n");
+		task_delay(DELAY);
+		/* for (int i = 0; i < 5; i++) { */
+		/* 	uart_puts("Task 0: Running... \n"); */
+		/* 	task_delay(DELAY); */
+		/* } */
+		/* uart_puts("Task 0: End ... \n"); */
 #ifdef USE_LOCK
 		spin_unlock();
 #endif
@@ -27,12 +58,12 @@ void user_task1(void)
 {
 	uart_puts("Task 1: Created!\n");
 	while (1) {
-		uart_puts("Task 1: Begin ... \n");
-		for (int i = 0; i < 5; i++) {
+		/* uart_puts("Task 1: Begin ... \n"); */
+		/* for (int i = 0; i < 5; i++) { */
 			uart_puts("Task 1: Running... \n");
 			task_delay(DELAY);
-		}
-		uart_puts("Task 1: End ... \n");
+		/* } */
+		/* uart_puts("Task 1: End ... \n"); */
 	}
 }
 
